f955bd36e639bb1a8886f7431e08ebd4d26ae6c0
Frilanser-of-UA <forestraws@gmail.com>
2026-02-25 22:33:03 +0200

    Fix lan traslate

--- #src/js/files/lang.js
+++ #src/js/files/lang.js
@@ -3,13 +3,37 @@ const translations = {
 	titleHero: { ru: "Привет, я <br/> Шупик Андрей", en: "Hi, I am <br/> Shupyk Andrii", ua: "Привіт, я <br/> Шупик Андрій" },
 	titleWork: { ru: "Мои проекты", en: "My projects", ua: "Мої проекти" },
 	titleAbout: { ru: "Обо мне", en: "About me", ua: "Про мене" },
+	titleContacts: { ru: "Контакты", en: "Contacts ", ua: "Контакти" },
 	titleForm: { ru: "Написать письмо", en: "Send mail", ua: "Написати листа" },
 
 	// text, labels
 	labelHero: { ru: "UI-розработчик (HTML, CSS, JavaScript, верстка)", en: "UI Developer (HTML, CSS, JavaScript)", ua: "UI-розробник (HTML, CSS, JavaScript, верстка)" },
 	textAbout: { ru: "Здравствуйте меня зовут Андрей, я UI-розработчик / верстальщик сайтов. Приветствую Вас на моем сайте.", en: "Hello, my name is Andrey, I am a UI developer (HTML, CSS, JavaScript). Welcome to my site.", ua: "Здравствуйте мене звати Андрій я UI-розробник / верстальник сайтів. Вітаю вас на моєму сайті" },
+	labelContacts: {
+		ru: "Свяжитесь со мной или подпишитесь на мои страницы в социальных сетях.", en: "Contact me or follow me on social media.", ua: "Зв'яжіться зі мною або підпишіться на мої сторінки в соціальних мережах."
+	},
 
-	// header nav links
+	// card title portfolio
+	aveCardTitleHome: { ru: "Главная", en: "Homepage", ua: "Головна" },
+	aveCardTitleBrand: { ru: "Бренд", en: "Brand", ua: "Бренд" },
+	aveCardTitleProduct: { ru: "Просмотр товара", en: "View product", ua: "Перегляд товару" },
+	aveCardTitleLookbook: { ru: "Обзоры товаров", en: "Product reviews", ua: "Огляди товарів" },
+	aveCardTitleLokal: { ru: "Местные магазины", en: "Local stores", ua: "Місцеві магазини" },
+	aveCardTitleForm: { ru: "Форма регистрации", en: "Registration form", ua: "Форма реєстрації" },
+
+	// card about text
+	aboutHtml: {
+		ru: "Html код валидный, структурированный . Применяю: блочную верстку, семантику, методологию BEM. Использую шаблонизатор для Html Nunjucks.", en: "Valid, structured HTML code. I apply block layout, semantics, and BEM methodology. I use Nunjucks as an HTML templating engine.", ua: "HTML - код валідний, структурований.Застосовую: блочну верстку, семантику, методологію BEM.Використовую шаблонізатор для HTML Nunjucks."
+	},
+	aboutCss: { ru: "Применяю: валидные кроссбраузерные свойства CSS3, flexbox, grid layout, анимации, responsive верстка. Так же работаю на препроцесоре SCSS.", en: "I apply valid cross-browser CSS3 properties, flexbox, grid layout, animations, and responsive design. I also work with the SCSS preprocessor.", ua: "Застосовую: валідні кросбраузерні властивості CSS3, flexbox, grid layout, анімації, responsive верстку. Також працюю на препроцесорі SCSS." },
+	aboutJs: { ru: "Применяю: различные плагины javascript, а так же написание чистого кода. Использую библиотеку jquery, а также различные плагины на ее базе. Добавление анимаций c помощью js.", en: "pply various JavaScript plugins and write clean code. I use the jQuery library as well as various plugins based on it. Adding animations using JS.", ua: "Застосовую: різноманітні плагіни JavaScript, а також написання чистого коду. Використовую бібліотеку jQuery та різні плагіни на її базі. Додавання анімацій за допомогою JS." },
+	aboutUi: { ru: "Работаю c макетами figma. А также: Adobe Photoshop с расширением psd. Работаю с различными другими графическими редакторами.", en: " I work with Figma layouts, as well as Adobe Photoshop (psd files). I am experienced with various other graphic editors.", ua: "Працюю з макетами Figma. А також: Adobe Photoshop з розширенням psd. Працюю з різними іншими графічними редакторами." },
+	aboutAther: { ru: "Работаю в редакторе кода VS Code. Работаю с Git, GitHub Сборщик проектов использую gulp", en: "I work in the VS Code editor. I use Git and GitHub. For project bundling, I use Gulp.", ua: "Працюю в редакторі коду VS Code. Працюю з Git, GitHub. Як збірник проєктів використовую Gulp." },
+	aboutUiTitle: { ru: "Дизайн макеты", en: "Design layouts", ua: "Дизайн-макети" },
+	aboutAtherTitle: { ru: "Другое", en: "Other", ua: "Інше" },
+
+
+	// header nav 
 	headerNav1: { ru: "Проекты", en: "Projects", ua: "Проекти" },
 	headerNav2: { ru: "Обо мне", en: "About me", ua: "Про мене" },
 	headerNav3: { ru: "Контакты", en: "Contacts", ua: "Контакти" },
@@ -53,6 +77,7 @@ const translations = {
 
 	// buttons
 	downloadHero: { ru: "Загрузить CV", en: "Download CV", ua: "Завантажити CV" },
+	buttonSee: { ru: "Посмотреть", en: "To watch", ua: "Подивитися" },
 };
 
 

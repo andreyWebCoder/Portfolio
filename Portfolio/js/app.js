819d56e735c78440b7b45da8d4cd57c61eca1efa
Frilanser-of-UA <forestraws@gmail.com>
2026-02-27 14:15:23 +0200

    Add animation on scroll

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -532,6 +532,47 @@ window.addEventListener("scroll", () => {
 goTop.addEventListener("click", () => {
 	window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+
+
+const animItems = document.querySelectorAll('._anim');
+console.log(animItems);
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let i = 0; i < animItems.length; i++) {
+			const animItem = animItems[i];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageXOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active')
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active')
+				}
+			}
+		}
+	}
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+
+}
 //Select
 let selects = document.getElementsByTagName('select');
 if (selects.length > 0) {
